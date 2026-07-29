12db53d6105a95f4378c36e26d6cc47b4358f4cc
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-07-29 13:32:16 +0600

    F3: Insert the textbox value into the chat-message-array which is stored into React State; Utilizes React State to track & store the value of textbox as well

--- 3-Chatbot/app/src/components/chatInput.jsx
+++ 3-Chatbot/app/src/components/chatInput.jsx
@@ -1,26 +1,46 @@
 import Button from "./button";
 import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 
 export default function ChatInput({ chats, setChats }) {
+  // Since the textbox initiates with empty value
+  const [inputText, setInputText] = useState("");
+
+  // This function gets invoked on-change event of the input-field
+  const handleSaveInputText = (event) => {
+    // Store textbox value into the React "State"
+    setInputText(event.target.value);
+  };
+
+  // Didn't define the function inside the `<Button />` component, rather placed it in such a common place where it can use other data/objects without getting them passed as arguments in the function parameter
   const handleSendMessage = () => {
-    console.log("Send Message!");
-    console.log(chats);
-    setChats([
-      ...chats,
-      {
-        message: "test",
-        sender: "user",
-        id: uuidv4(),
-      },
-    ]);
+    if (inputText.trim() !== "") {
+      // Create a new array of chat messages
+      setChats([
+        ...chats,
+        {
+          message: inputText.trim(),
+          sender: "user",
+          id: uuidv4(),
+        },
+      ]);
+    }
+
+    // Reset the state after sending
+    setInputText("");
   };
 
   return (
     <>
       <div>
-        <input type="text" placeholder="Send a message to chatbot" />
-        <Button name="Send" />
-        <Button name="Send Message" handleSendMessage={handleSendMessage} />
+        <input
+          type="text"
+          placeholder="Send a message to chatbot"
+          value={inputText}
+          onChange={handleSaveInputText}
+          onKeyDown={(event) => event.keyCode === 13 && handleSendMessage()}
+        />
+        <Button name="Send" handleSendMessage={handleSendMessage} />
       </div>
     </>
   );
