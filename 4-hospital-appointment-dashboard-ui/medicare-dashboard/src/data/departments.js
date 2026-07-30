3a19ce790a206f529652e69af3953f857a50de73
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-07-31 05:32:15 +0600

    F4: Generate synthetic data using ChatGPT regarding doctors, departments & appointments

---
+++ 4-hospital-appointment-dashboard-ui/medicare-dashboard/src/data/departments.js
@@ -0,0 +1,66 @@
+export default departments = [
+  {
+    id: 1,
+    departmentName: "Cardiology",
+  },
+  {
+    id: 2,
+    departmentName: "Neurology",
+  },
+  {
+    id: 3,
+    departmentName: "Dermatology",
+  },
+  {
+    id: 4,
+    departmentName: "Orthopedics",
+  },
+  {
+    id: 5,
+    departmentName: "Gynecology",
+  },
+  {
+    id: 6,
+    departmentName: "Pediatrics",
+  },
+  {
+    id: 7,
+    departmentName: "ENT",
+  },
+  {
+    id: 8,
+    departmentName: "Ophthalmology",
+  },
+  {
+    id: 9,
+    departmentName: "General Medicine",
+  },
+  {
+    id: 10,
+    departmentName: "Psychiatry",
+  },
+  {
+    id: 11,
+    departmentName: "Nephrology",
+  },
+  {
+    id: 12,
+    departmentName: "Urology",
+  },
+  {
+    id: 13,
+    departmentName: "Endocrinology",
+  },
+  {
+    id: 14,
+    departmentName: "Pulmonology",
+  },
+  {
+    id: 15,
+    departmentName: "Gastroenterology",
+  },
+  {
+    id: 16,
+    departmentName: "Oncology",
+  },
+];
