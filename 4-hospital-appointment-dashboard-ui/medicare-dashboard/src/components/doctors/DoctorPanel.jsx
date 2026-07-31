24c7f425ecd6062c554b65bd89313dda5a599c64
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-07-31 23:08:35 +0600

    F4: Created the draft styling of the header of `DoctorPanel` component; Doctors' count pill is rendering static data here

---
+++ 4-hospital-appointment-dashboard-ui/medicare-dashboard/src/components/doctors/DoctorPanel.jsx
@@ -0,0 +1,10 @@
+export default function DoctorPanel() {
+  return (
+    <>
+      <div className="doctor-panel-header">
+        <h3 className="card-title">Our Doctors</h3>
+        <p className="count-pill">8</p>
+      </div>
+    </>
+  );
+}
--- 4-hospital-appointment-dashboard-ui/medicare-dashboard/src/components/layout/MainGrid.jsx
+++ 4-hospital-appointment-dashboard-ui/medicare-dashboard/src/components/layout/MainGrid.jsx
@@ -1,8 +1,12 @@
+import DoctorPanel from "../doctors/DoctorPanel";
+
 export default function MainGrid() {
   return (
     <div className="main-grid">
       {/* Doctor List, Search & Filter Container */}
-      <div className="main-grid-left-container"></div>
+      <div className="main-grid-left-container">
+        <DoctorPanel />
+      </div>
       {/* Appointment Form & List Container */}
       <div className="main-grid-right-container"></div>
     </div>
