c5e2c25b913f0e5587a3a9747a8c7c6afe6ccc1b
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-08-03 07:03:57 +0600

    F4: Moved the department-chip's `onClick` handler function to one-step above in the DOM tree to `DoctorPanel` component, thus doctors data can be filtered by departments & can be shared to `DoctorList` component

--- 4-hospital-appointment-dashboard-ui/medicare-dashboard/src/components/doctors/DoctorFilter.jsx
+++ 4-hospital-appointment-dashboard-ui/medicare-dashboard/src/components/doctors/DoctorFilter.jsx
@@ -1,33 +1,16 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Departments from "../../data/departments";
 import DoctorSearch from "./doctorFilter/DoctorSearch";
 import DepartmentFilter from "./doctorFilter/DepartmentFilter";
 
-export default function DoctorFilter({ doctors }) {
+export default function DoctorFilter({ onClick, ref }) {
   const [inputValue, setInputValue] = useState("");
   const [departments, setDepartments] = useState(Departments);
-  const departmentFilterContainerRef = useRef(null);
-
-  const handleFilterDoctorsByDepartment = (event) => {
-    const departmentFilterContainerElem = departmentFilterContainerRef.current;
-
-    departmentFilterContainerElem.childNodes.forEach((dptChip) => {
-      if (dptChip.className === "department-filter-chip-selected") {
-        dptChip.className = "department-filter-chip";
-      }
-    });
-
-    event.target.className = "department-filter-chip-selected";
-  };
 
   return (
     <div className="doctor-list-filter-container">
       <DoctorSearch inputValue={inputValue} setInputValue={setInputValue} />
-      <DepartmentFilter
-        departments={departments}
-        onClick={handleFilterDoctorsByDepartment}
-        ref={departmentFilterContainerRef}
-      />
+      <DepartmentFilter departments={departments} onClick={onClick} ref={ref} />
     </div>
   );
 }
--- 4-hospital-appointment-dashboard-ui/medicare-dashboard/src/components/doctors/DoctorPanel.jsx
+++ 4-hospital-appointment-dashboard-ui/medicare-dashboard/src/components/doctors/DoctorPanel.jsx
@@ -1,12 +1,33 @@
+import { useRef } from "react";
 import DoctorFilter from "./DoctorFilter";
 import DoctorList from "./DoctorList";
 import DoctorPanelHeader from "./doctorPanel/DoctorPanelHeader";
 
 export default function DoctorPanel({ doctors }) {
+  const departmentFilterContainerRef = useRef(null);
+
+  const handleFilterDoctorsByDepartment = (event) => {
+    const departmentFilterContainerElem = departmentFilterContainerRef.current;
+
+    departmentFilterContainerElem.childNodes.forEach((dptChip) => {
+      if (dptChip.className === "department-filter-chip-selected") {
+        dptChip.className = "department-filter-chip";
+      }
+    });
+
+    event.target.className = "department-filter-chip-selected";
+
+    console.log(event.target.innerText);
+  };
+
   return (
     <div className="doctor-panel">
       <DoctorPanelHeader doctors={doctors} />
-      <DoctorFilter doctors={doctors} />
+      <DoctorFilter
+        doctors={doctors}
+        onClick={handleFilterDoctorsByDepartment}
+        ref={departmentFilterContainerRef}
+      />
       <DoctorList />
     </div>
   );
