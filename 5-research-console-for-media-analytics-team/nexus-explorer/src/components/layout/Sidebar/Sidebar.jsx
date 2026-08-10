5bd1edb95c612e6bea5ff81bebfed6891c2e207f
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-08-10 21:40:43 +0600

    F5: Implemented the styling of the sidebar including the toggle of active/inactive state styling of the navlinks;

--- 5-research-console-for-media-analytics-team/nexus-explorer/src/components/layout/Sidebar/Sidebar.jsx
+++ 5-research-console-for-media-analytics-team/nexus-explorer/src/components/layout/Sidebar/Sidebar.jsx
@@ -1,6 +1,12 @@
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Sidebar.module.css";
 import NexusExplorerLogo from "../../../asset/nexus-explorer-logo.png";
+import DashboardIcon from "../../../asset/navIcons/dashboard.png";
+import CharactersIcon from "../../../asset/navIcons/characters.png";
+import EpisodesIcon from "../../../asset/navIcons/episodes.png";
+import LocationsIcon from "../../../asset/navIcons/locations.png";
+import WatchlistIcon from "../../../asset/navIcons/watchlist.png";
+import SettingsIcon from "../../../asset/navIcons/settings.png";
 
 export default function Sidebar() {
   return (
@@ -16,36 +22,156 @@ export default function Sidebar() {
         <div className="exploreContainer">
           <p className={styles.navLinksGroupHeader}>Explore</p>
           <ul className={styles.navLinksList}>
-            <li className="navLink">
-              <NavLink to="/">Dashboard</NavLink>
-            </li>
-            <li className="navLink">
-              <NavLink to="/characters">Characters</NavLink>
-            </li>
-            <li className="navLink">
-              <NavLink to="/episodes">Episodes</NavLink>
-            </li>
-            <li className="navLink">
-              <NavLink to="/locations">Locations</NavLink>
-            </li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.activeLink : styles.inactiveLink}`
+              }
+            >
+              <li>
+                <img
+                  src={DashboardIcon}
+                  alt="Dashboard Icon"
+                  className={styles.navLinkIcon}
+                />
+                <span className={styles.navLinkText}>Dashboard</span>
+              </li>
+            </NavLink>
+            <NavLink
+              to="/characters"
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.activeLink : styles.inactiveLink}`
+              }
+            >
+              {({ isActive }) => (
+                <li>
+                  <img
+                    src={CharactersIcon}
+                    alt="Characters"
+                    className={styles.navLinkIcon}
+                  />
+                  <span className={styles.navLinkText}>Characters</span>
+
+                  <span
+                    className={
+                      isActive
+                        ? `${styles.countPill} ${styles.countPillActive}`
+                        : styles.countPill
+                    }
+                  >
+                    826
+                  </span>
+                </li>
+              )}
+            </NavLink>
+            <NavLink
+              to="/episodes"
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.activeLink : styles.inactiveLink}`
+              }
+            >
+              {({ isActive }) => (
+                <li>
+                  <img
+                    src={EpisodesIcon}
+                    alt="Episodes Icon"
+                    className={styles.navLinkIcon}
+                  />
+                  <span className={styles.navLinkText}>Episodes</span>
+
+                  <span
+                    className={
+                      isActive
+                        ? `${styles.countPill} ${styles.countPillActive}`
+                        : styles.countPill
+                    }
+                  >
+                    51
+                  </span>
+                </li>
+              )}
+            </NavLink>
+            <NavLink
+              to="/locations"
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.activeLink : styles.inactiveLink}`
+              }
+            >
+              {({ isActive }) => (
+                <li>
+                  <img
+                    src={LocationsIcon}
+                    alt="Locations Icon"
+                    className={styles.navLinkIcon}
+                  />
+                  <span className={styles.navLinkText}>Locations</span>
+
+                  <span
+                    className={
+                      isActive
+                        ? `${styles.countPill} ${styles.countPillActive}`
+                        : styles.countPill
+                    }
+                  >
+                    126
+                  </span>
+                </li>
+              )}
+            </NavLink>
           </ul>
         </div>
         <div className="personalContainer">
           <p className={styles.navLinksGroupHeader}>Personal</p>
           <ul className={styles.navLinksList}>
-            <li className="navLink">
-              <NavLink to="/watchlist">Watchlist</NavLink>
-            </li>
-            <li className="navLink">
-              <NavLink to="/settings">Settings</NavLink>
-            </li>
+            <NavLink
+              to="/watchlist"
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.activeLink : styles.inactiveLink}`
+              }
+            >
+              {({ isActive }) => (
+                <li>
+                  <img
+                    src={WatchlistIcon}
+                    alt="Watchlist Icon"
+                    className={styles.navLinkIcon}
+                  />
+                  <span className={styles.navLinkText}>Watchlist</span>
+
+                  <span
+                    className={
+                      isActive
+                        ? `${styles.countPill} ${styles.countPillActive}`
+                        : styles.countPill
+                    }
+                  >
+                    4
+                  </span>
+                </li>
+              )}
+            </NavLink>
+            <NavLink
+              to="/settings"
+              className={({ isActive }) =>
+                `${styles.link} ${isActive ? styles.activeLink : styles.inactiveLink}`
+              }
+            >
+              <li>
+                <img
+                  src={SettingsIcon}
+                  alt="Settings Icon"
+                  className={styles.navLinkIcon}
+                />
+                <span className={styles.navLinkText}>Settings</span>
+              </li>
+            </NavLink>
           </ul>
         </div>
       </div>
       <div className={styles.cacheStatusBoxContainer}>
-        <p>React Query Cache</p>
-        <span>12 queries cached • 2 stale</span>
-        <span>Background refetch: on focus</span>
+        <p className={styles.reactQueryCacheHeader}>React Query Cache</p>
+        <p>12 queries cached • 2 stale</p>
+        <p>Background refetch: on focus</p>
       </div>
     </div>
   );
