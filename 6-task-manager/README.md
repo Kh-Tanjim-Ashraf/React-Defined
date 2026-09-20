133c6ff2d73f872a32c430cde4234afd310ffe2a
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-09-20 13:12:41 +0600

    F6: Initialize the folder structure of `Task Management` dashboard; Integrated Tailwind CSS

---
+++ 6-task-manager/README.md
@@ -0,0 +1,3 @@
+# Task Manager
+
+The frontend consumes data from a task manager API server, initially it can handle CRUD operations on task. The tasks are initially displayed as a list under a task form. A user can create a new task by submitting the form & can see the task appeared in the list below.
---
+++ 6-task-manager/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="bg-slate-50 h-screen flex flex-col">
+      <header className="h-20 bg-amber-100">
+        <h1 className="text-6xl">Homepage</h1>
+      </header>
+      <main className="flex-1">
+        <p className="text-slate-700">Main Content</p>
+      </main>
+      <footer className="bg-slate-200 h-10">
+        <p className="text-slate-600">Sitemap</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
