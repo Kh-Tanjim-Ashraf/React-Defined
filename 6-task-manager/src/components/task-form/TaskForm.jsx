0e88e2df117741fab18a70c1bf5d3a89006c0fd1
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-09-22 01:37:19 +0600

    F6: Updated the design layout of new task creation form; Implemented the `Theme Variables` to include color palettes

--- 6-task-manager/src/components/task-form/TaskForm.jsx
+++ 6-task-manager/src/components/task-form/TaskForm.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 export default function TaskForm({ onAdd }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [status, setStatus] = useState("");
-  const [priority, setPriority] = useState("");
+  const [status, setStatus] = useState("todo");
+  const [priority, setPriority] = useState("low");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,56 +18,101 @@ export default function TaskForm({ onAdd }) {
     // Clear the form input fields after form submission
     setTitle("");
     setDescription("");
-    setStatus("");
-    setPriority("");
+    setStatus("todo");
+    setPriority("low");
+  };
+
+  const handleStatusChange = (selectedStatus) => {
+    setStatus(selectedStatus);
+  };
+
+  const handlePriorityChange = (selectedPriority) => {
+    setPriority(selectedPriority);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="bg-emerald-200 py-6 flex flex-col">
-      <h2 className="text-2xl font-semibold text-slate-500 text-center">
+    <form
+      onSubmit={handleSubmit}
+      className=" bg-white py-6 flex flex-col gap-4"
+    >
+      <h2 className="text-2xl text-shadow-grey-600 self-center">
         New Task Form
       </h2>
-      <div className="form-group flex justify-center">
-        <label htmlFor="task">Task</label>
-        <input
-          id="task"
-          type="text"
-          className="rounded border border-black focus:outline-black"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          autoComplete="off"
-        />
+      <div className="form-group flex gap-4">
+        {/* Task Name */}
+        <div className="flex-1 flex flex-col">
+          <label htmlFor="task" className="text-sm text-shadow-grey-500">
+            Task
+          </label>
+          <input
+            id="task"
+            type="text"
+            className="rounded text-shadow-grey-800 placeholder:font-light placeholder-shadow-grey-200 focus:placeholder:font-light border-[.2px] border-shadow-grey-100 focus:outline-[.2px] focus:outline-shadow-grey-200"
+            placeholder="Task name..."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            autoComplete="off"
+          />
+        </div>
+        {/* Task Status */}
+        <div className="flex flex-col">
+          <label htmlFor="status" className="text-sm text-shadow-grey-500">
+            Status
+          </label>
+          <select
+            id="status"
+            className="rounded text-shadow-grey-800 border-[.2px] border-shadow-grey-100 focus:outline-[.2px] focus:outline-shadow-grey-200"
+            value={status}
+            onChange={(e) => handleStatusChange(e.target.value)}
+            autoComplete="off"
+          >
+            <option value="todo" className="text-shadow-grey-700">
+              Todo
+            </option>
+            <option value="in_progress" className="text-shadow-grey-700">
+              In Progress
+            </option>
+            <option value="done" className="text-shadow-grey-700">
+              Done
+            </option>
+          </select>
+        </div>
+        {/* Task Priority */}
+        <div className="flex flex-col">
+          <label htmlFor="priority" className="text-sm text-shadow-grey-500">
+            Priority
+          </label>
+          <select
+            id="priority"
+            className="rounded text-shadow-grey-800 border-[.2px] border-shadow-grey-100 focus:outline-[.2px] focus:outline-shadow-grey-200"
+            value={priority}
+            onChange={(e) => handlePriorityChange(e.target.value)}
+            autoComplete="off"
+          >
+            <option value="low" className="text-shadow-grey-700">
+              Low
+            </option>
+            <option value="medium" className="text-shadow-grey-700">
+              Medium
+            </option>
+            <option value="high" className="text-shadow-grey-700">
+              High
+            </option>
+          </select>
+        </div>
       </div>
-      <div className="form-group flex justify-center">
-        <label htmlFor="description">Description</label>
-        <input
+      {/* Task Description */}
+      <div className="form-group flex flex-col">
+        <label htmlFor="description" className="text-sm text-shadow-grey-500">
+          Description
+        </label>
+        <textarea
           id="description"
           type="textarea"
-          className="rounded border border-black focus:outline-black"
+          className="h-32 w-auto resize-none overflow-scroll scrollbar-none rounded text-shadow-grey-800 placeholder:font-light placeholder-shadow-grey-200 focus:placeholder:font-light border-[.2px] border-shadow-grey-100 focus:outline-[.2px] focus:outline-shadow-grey-200"
+          placeholder="What needs to be done?"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          autoComplete="off"
-        />
-      </div>
-      <div className="form-group flex justify-center">
-        <label htmlFor="status">Status</label>
-        <input
-          id="status"
-          type="text"
-          className="rounded border border-black focus:outline-black"
-          value={status}
-          onChange={(e) => setStatus(e.target.value)}
-          autoComplete="off"
-        />
-      </div>
-      <div className="form-group flex justify-center">
-        <label htmlFor="priority">Priority</label>
-        <input
-          id="priority"
-          type="text"
-          className="rounded border border-black focus:outline-black"
-          value={priority}
-          onChange={(e) => setPriority(e.target.value)}
           autoComplete="off"
         />
       </div>
