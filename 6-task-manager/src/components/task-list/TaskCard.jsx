f1b006d2df1ff60c0099d3d47c0dad66dd1185ef
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-09-21 14:13:17 +0600

    F6: [Design] Defined the minimum & maximum width of task cards

--- 6-task-manager/src/components/task-list/TaskCard.jsx
+++ 6-task-manager/src/components/task-list/TaskCard.jsx
@@ -1,6 +1,6 @@
 export default function TaskCard({ task }) {
   return (
-    <div className="mx-auto bg-slate-100 p-2 rounded-xl max-w-md">
+    <div className="mx-auto bg-slate-100 p-2 rounded-xl min-w-lg max-w-md">
       <h3 className="text-lg font-semibold">{task.title}</h3>
       <p className="text-xs text-slate-700">{task.description}</p>
       <div className="flex mt-2 gap-3 justify-end">
