298634110823fdb44545ea1c9d7c62ee2f9004ca
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-09-23 11:50:03 +0600

    [Design] Updated the design of the application footer; Resized the application's heading text

--- 6-task-manager/src/layout/Footer.jsx
+++ 6-task-manager/src/layout/Footer.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
-    <footer className="bg-shadow-grey-100 h-10">
-      <p className="text-vintage-grape-500 text-center">Sitemap</p>
+    <footer className="bg-shadow-grey-100 h-14 flex justify-center items-center">
+      <p className="text-xs text-shadow-grey-400">
+        <Link to="/tasks">Task Management System</Link> • Made by{" "}
+        <a
+          href="https://www.github.com/Kh-Tanjim-Ashraf"
+          target="_blank"
+          className="font-medium underline underline-offset-4 hover:text-slate-600"
+        >
+          Kh Tanjim Ashraf
+        </a>
+      </p>
     </footer>
   );
 }
--- 6-task-manager/src/layout/Header.jsx
+++ 6-task-manager/src/layout/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="h-20 border-shadow-grey-100 py-6">
-      <h1 className="text-6xl text-center text-shadow-grey-700">
+      <h1 className="text-4xl text-center text-shadow-grey-700">
         Task Management System
       </h1>
     </header>
